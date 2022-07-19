19d9a18ae642bc3b20926b036c8f5aa766f3779b
RobertoGzzMtz <robertogzzmtz@gmail.com>
2022-07-19 09:28:30 -0500

    initial Commit

---
+++ src/api.js
@@ -0,0 +1,13 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "da4be94abamshe7b7a2e6d6f9236p1f9eeajsn87f8b43c9468",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+
+export const API_KEY = "819c01dbc5587589d23cc73e2a784cb0";
